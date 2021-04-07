3c4ed397c83f91c4feb5b96d9b587002cedf7849
Sachin Rajbhandari <sachink.rb@gmail.com>
2021-04-06 20:07:18 -0500

    Navigation Part 1

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,8 +1,31 @@
 import React, { Component } from 'react';
 import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
-import { View } from 'react-native';
+import { View, Platform } from 'react-native';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createAppContainer } from 'react-navigation';
 import { CAMPSITES } from '../shared/campsites';
+
+const DirectoryNavigator = createStackNavigator(
+    {
+        Directory: { screen: Directory },
+        CampsiteInfo: { screen: CampsiteInfo }
+    },
+    {
+        initialRouteName: 'Directory',
+        defaultNavigationOptions: {
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            }
+        }
+    }
+);
+
+const AppNavigator = createAppContainer(DirectoryNavigator);
 
 class Main extends Component {
     constructor(props) {
@@ -19,15 +42,12 @@ class Main extends Component {
 
     render() {
         return (
-            <View style={{ flex: 1 }}>
-                <Directory
-                    campsites={this.state.campsites}
-                    onPress={campsiteId => this.onCampsiteSelect(campsiteId)}
-                />
-                <CampsiteInfo
-                    campsite={this.state.campsites.filter(
-                        campsite => campsite.id === this.state.selectedCampsite)[0]}
-                />
+            <View
+                style={{
+                    flex: 1,
+                    paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight
+                }}>
+                <AppNavigator />
             </View>
         );
     }
