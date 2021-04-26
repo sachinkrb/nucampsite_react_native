cacf79d93bd869f5ef071be0cd47ad419ae04405
Sachin Rajbhandari <sachink.rb@gmail.com>
2021-04-26 11:49:54 -0500

    End of week 3

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -33,6 +33,7 @@ function RenderCampsite(props) {
   const { campsite } = props;
   const view = React.createRef();
   const recognizeDrag = ({ dx }) => (dx < -200 ? true : false);
+  const recognizeComment = ({ dx }) => (dx > 200 ? true : false);
   const panResponder = PanResponder.create({
     onStartShouldSetPanResponder: () => true,
     onPanResponderGrant: () => {
@@ -64,6 +65,8 @@ function RenderCampsite(props) {
           ],
           { cancelable: false }
         );
+      } else if (recognizeComment(gestureState)) {
+        props.onShowModal();
       }
       return true;
     },
